8f5ee7532490c4f5c6b35b8eba0c7091c381a230
Luiz Lopes <lopesluiz_@hotmail.com>
2022-06-02 08:44:20 -0300

    init openclose principle commit

--- src/solid/OpenClosedPrinciple/Program.cs
+++ src/solid/OpenClosedPrinciple/Program.cs
@@ -6,7 +6,55 @@ namespace OpenClosedPrinciple
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var draw = DrawShape(Shape.Square);
+
+            Console.WriteLine(draw.Drawing());
+        }
+
+        private static IDraw DrawShape(Shape shape)
+        {
+            switch (shape)
+            {
+                case Shape.Circle:
+                    return new Circle();
+                case Shape.Square:
+                    return new Square();                
+                default:
+                    return new Square();
+            }
+        }
+    }
+
+    public enum Shape
+    {
+        None,
+        Square,
+        Circle
+    }
+
+    public interface IDraw
+    {
+        string Drawing();
+    }
+
+    public abstract class Draw : IDraw
+    {
+        public abstract string Drawing();
+    }
+
+    public class Square : IDraw
+    {
+        public string Drawing()
+        {
+            return $"Drawing a square";
+        }
+    }
+
+    public class Circle : IDraw
+    {
+        public string Drawing()
+        {
+            return $"Drawing a circle";
         }
     }
 }
