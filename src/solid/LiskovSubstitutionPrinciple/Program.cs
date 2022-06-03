f149dc747260fd8a49811760266fd0696e59ff0c
Luiz Lopes <lopesluiz_@hotmail.com>
2022-06-03 06:48:30 -0300

    dependency inversion and liskov substitution

---
+++ src/solid/LiskovSubstitutionPrinciple/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace LiskovSubstitutionPrinciple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public interface IPrinterTasks
+    {
+        void Print(string PrintContent);
+        void Scan(string ScanContent);
+    }
+    public interface IFaxTasks
+    {
+        void Fax(string content);
+    }
+    public interface IPrintDuplexTasks
+    {
+        void PrintDuplex(string content);
+    }
+
+    public class HPLaserJetPrinter : IPrinterTasks, IFaxTasks, 
+                                     IPrintDuplexTasks
+    {
+        public void Print(string PrintContent)
+        {
+            Console.WriteLine("Print Done");
+        }
+
+        public void Scan(string ScanContent)
+        {
+            Console.WriteLine("Scan content");
+        }
+
+        public void Fax(string FaxContent)
+        {
+            Console.WriteLine("Fax content");
+        }
+
+        public void PrintDuplex(string PrintDuplexContent)
+        {
+            Console.WriteLine("Print Duplex content");
+        }
+    }
+
+    public class LiquidInkjetPrinter : IPrinterTasks
+    {
+        public void Print(string PrintContent)
+        {
+            Console.WriteLine("Print Done");
+        }
+
+        public void Scan(string ScanContent)
+        {
+            Console.WriteLine("Scan content");
+        }
+    }
+}
